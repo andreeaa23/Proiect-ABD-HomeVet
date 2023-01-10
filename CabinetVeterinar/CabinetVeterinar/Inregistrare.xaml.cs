33e927620c58a1e6082025607c5006fac6d4f69a
daniel.istrate <daniel.istrate@mta.ro>
2023-01-10 19:43:19 +0200

    ..

--- CabinetVeterinar/CabinetVeterinar/Inregistrare.xaml.cs
+++ CabinetVeterinar/CabinetVeterinar/Inregistrare.xaml.cs
@@ -22,20 +22,26 @@ namespace CabinetVeterinar
         public Inregistrare()
         {
             InitializeComponent();
+            LoadTip();
         }
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
                 DragMove();
         }
-       
+        public void LoadTip()
+        {
+            cbTip.Items.Add("User");
+            cbTip.Items.Add("Medic");
+            cbTip.Items.Add("Asistent");
+        }
         private void btnLogare_Click_1(object sender, RoutedEventArgs e)
         {
             string nume = txtNume.Text;
             string prenume = txtPrenume.Text;
             string email = txtEmail.Text;
             string parola = txtParola.Text;
-            string tip = txtTip.Text;
+            string tip = cbTip.SelectedItem.ToString().Substring(0,1);
             DateTime data = DateTime.Now;
 
             var context = new HomeVetEntities1();
